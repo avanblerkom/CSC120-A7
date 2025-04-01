19cd622c2a96f00c2e78b7489de9ee6a335fca60
Alex Van Blerkom <avanblerkom@smith.edu>
2025-04-01 13:05:43 -0400

    done?

--- Building.java
+++ Building.java
@@ -1,46 +1,82 @@
+/**
+ * This class represents a Building, which serves as a base class for other types of buildings.
+ * It includes attributes for the building's name, address, and number of floors.
+ */
 public class Building {
 
-    protected String name = "<Name Unknown>";
-    protected String address = "<Address Unknown>";
-    protected int nFloors = 1;
+    protected String name = "<Name Unknown>"; // Name of the building
+    protected String address = "<Address Unknown>"; // Address of the building
+    protected int nFloors = 1; // Number of floors in the building
 
+    /**
+     * Constructor to initialize a Building with the specified name, address, and number of floors.
+     * @param name The name of the building.
+     * @param address The address of the building.
+     * @param nFloors The number of floors in the building.
+     * @throws RuntimeException if the number of floors is less than 1.
+     */
     public Building(String name, String address, int nFloors) {
-        if (name != null) { this.name = name; }
-        if (address != null) { this.address = address; } 
+        if (name != null) {
+            this.name = name;
+        }
+        if (address != null) {
+            this.address = address;
+        }
         if (nFloors < 1) {
             throw new RuntimeException("Cannot construct a building with fewer than 1 floor.");
         }
         this.nFloors = nFloors;
     }
 
-    
-    
+    /**
+     * Constructor to initialize a Building using a House object.
+     * @param h The House object to copy attributes from.
+     */
     public Building(House h) {
-        //TODO Auto-generated constructor stub
         this.name = h.getName();
         this.address = h.getAddress();
         this.nFloors = h.getFloors();
     }
 
+    /**
+     * Gets the name of the building.
+     * @return The name of the building.
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Gets the address of the building.
+     * @return The address of the building.
+     */
     public String getAddress() {
         return this.address;
     }
 
+    /**
+     * Gets the number of floors in the building.
+     * @return The number of floors in the building.
+     */
     public int getFloors() {
         return this.nFloors;
     }
 
+    /**
+     * Returns a string representation of the building.
+     * @return A string describing the building.
+     */
+    @Override
     public String toString() {
         return this.name + " is a " + this.nFloors + "-story building located at " + this.address;
     }
 
+    /**
+     * Main method to test the Building class.
+     * @param args Command-line arguments (not used).
+     */
     public static void main(String[] args) {
         Building fordHall = new Building("Ford Hall", "100 Green Street Northampton, MA 01063", 4);
         System.out.println(fordHall);
     }
-
 }
--- House.java
+++ House.java
@@ -1,58 +1,85 @@
-/* This is a stub for the House class */
-
+/**
+ * This class represents a House, which extends the Building class and implements HouseRequirements.
+ * It manages residents and provides methods to move in, move out, and check residency.
+ */
 import java.util.ArrayList;
 
-public class House extends Building {
+public class House extends Building implements HouseRequirements {
 
-  private ArrayList<Student> residents; // List of residents
-  private boolean hasDiningRoom; // Indicates if the house has a dining room
+    private ArrayList<Student> residents; // List of residents
+    private boolean hasDiningRoom; // Indicates if the house has a dining room
 
-  // Constructor to initialize the House
-  public House(String name, String address, int nFloors, boolean hasDiningRoom) {
-    super(name, address, nFloors); // Call the Building constructor
-    this.residents = new ArrayList<Student>(); // Initialize the residents list
-    this.hasDiningRoom = hasDiningRoom; // Set the dining room status
-    System.out.println("You have built a house: 🏠");
-  }
-
-  public boolean hasDiningRoom() {
-    return this.hasDiningRoom;
-  }
-
-  public int nResidents() {
-    return this.residents.size();
-  }
-
-  // Method to move a student into the house
-  public void moveIn(Student s) {
-    if (!this.residents.contains(s)) { // Check if the student is not already a resident
-      this.residents.add(s); // Add the student to the residents list
-      System.out.println(s.getName() + " has moved in.");
-    } else {
-      System.out.println(s.getName() + " is already a resident.");
+    /**
+     * Constructor to initialize the House.
+     * @param name The name of the house.
+     * @param address The address of the house.
+     * @param nFloors The number of floors in the house.
+     * @param hasDiningRoom Whether the house has a dining room.
+     */
+    public House(String name, String address, int nFloors, boolean hasDiningRoom) {
+        super(name, address, nFloors); // Call the Building constructor
+        this.residents = new ArrayList<Student>(); // Initialize the residents list
+        this.hasDiningRoom = hasDiningRoom; // Set the dining room status
+        System.out.println("You have built a house: 🏠");
     }
-  }
 
-  // Method to move a student out of the house
-  public Student moveOut(Student s) {
-    if (this.residents.remove(s)) { // Remove the student if they are a resident
-      System.out.println(s.getName() + " has moved out.");
-      return s; // Return the student who moved out
-    } else {
-      System.out.println(s.getName() + " is not a resident.");
-      return null; // Return null if the student was not a resident
+    /**
+     * Checks if the house has a dining room.
+     * @return True if the house has a dining room, false otherwise.
+     */
+    public boolean hasDiningRoom() {
+        return this.hasDiningRoom;
     }
-  }
 
-  // Method to check if a student is a resident of the house
-  public boolean isResident(Student s) {
-    return this.residents.contains(s); // Check if the student is in the residents list
-  }
+    /**
+     * Gets the number of residents in the house.
+     * @return The number of residents.
+     */
+    public int nResidents() {
+        return this.residents.size();
+    }
 
-  public static void main(String[] args) {
-    House myHouse = new House("Maple House", "123 Maple St", 3, true);
-    System.out.println("Dining room: " + myHouse.hasDiningRoom());
-    System.out.println("Number of residents: " + myHouse.nResidents());
-  }
+    /**
+     * Moves a student into the house.
+     * @param s The student to move in.
+     */
+    public void moveIn(Student s) {
+        if (!this.residents.contains(s)) { // Check if the student is not already a resident
+            this.residents.add(s); // Add the student to the residents list
+            System.out.println(s.getName() + " has moved in.");
+        } else {
+            System.out.println(s.getName() + " is already a resident.");
+        }
+    }
+
+    /**
+     * Moves a student out of the house.
+     * @param s The student to move out.
+     * @return The student who moved out, or null if the student was not a resident.
+     */
+    public Student moveOut(Student s) {
+        if (this.residents.remove(s)) { // Remove the student if they are a resident
+            System.out.println(s.getName() + " has moved out.");
+            return s; // Return the student who moved out
+        } else {
+            System.out.println(s.getName() + " is not a resident.");
+            return null; // Return null if the student was not a resident
+        }
+    }
+
+    /**
+     * Checks if a student is a resident of the house.
+     * @param s The student to check.
+     * @return True if the student is a resident, false otherwise.
+     */
+    public boolean isResident(Student s) {
+        return this.residents.contains(s); // Check if the student is in the residents list
+    }
+
+    public static void main(String[] args) {
+        House myHouse = new House("Maple House", "123 Maple St", 3, true);
+        System.out.println("Dining room: " + myHouse.hasDiningRoom());
+        System.out.println("Number of residents: " + myHouse.nResidents());
+    }
 }
 
